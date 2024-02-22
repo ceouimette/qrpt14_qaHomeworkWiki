34cb0e7be769dd06294641cdd10a5ba6accc9eb0
ceouimette <ceouimette@gmail.com>
2024-02-21 18:45:44 -0700

    selectors and page objects 2.6 homework

--- 2.6/2.6 homework/emWithPage.test.ts
+++ 2.6/2.6 homework/emWithPage.test.ts
@@ -1,22 +1,26 @@
-import { employeeManager } from "./employeeManagerPage";
-import { Builder, Capabilities, WebDriver } from "selenium-webdriver";
+import { Builder, Capabilities, WebDriver, WebElement } from "selenium-webdriver";
 const chromedriver =require("chromedriver");
 const driver: WebDriver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
+import { employeeManager } from "./employeeManagerPage";
 const employMan = new employeeManager(driver);
+
 test("employee manager works", async () => {
     await employMan.navigate();
     await employMan.click(employMan.bernice);
     await employMan.sendKeys(employMan.nameInput, "Bob Teller");
     await employMan.click(employMan.saveButton);
+    //^ changeing bernice name
     await employMan.click(employMan.phillip);
     await employMan.sendKeys(employMan.titleInput, "CEO");
     await employMan.click(employMan.saveButton);
+    //^ changing phillip title
     await employMan.click(employMan.addEmployee);
     await employMan.click(employMan.newEmployee);
     await employMan.sendKeys(employMan.nameInput, "ReinHart");
     await employMan.sendKeys(employMan.titleInput, "Tank");
     await employMan.click(employMan.saveButton);
+    //^ adding new employee
     await employMan.driver.sleep(10000);
     await employMan.driver.quit();
 });
